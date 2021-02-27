8a5bf3c52e267c6680024cd818ad1f1e1cb0d9d8
shiv122 <37039611+shiv122@users.noreply.github.com>
2021-02-27 17:36:50 +0530

    modulate code and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.style.css";
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+export default HomePage;
